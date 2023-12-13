50f25e043286d454d3f6f553fdeee89d11d7cbd8
Vinicius Candeia <viniciuscandeia12@gmail.com>
2023-12-13 15:17:18 -0300

    Fim do exercício!

--- aula/ex01.ex
+++ aula/ex01.ex
@@ -4,10 +4,28 @@ defmodule Lasagna do
   def expected_minutes_to_oven(), do: 40
 
   # Tempo restante no forno
-  def remaining_minutes_in_oven(tempoAtual) do
-    expected_minutes_to_oven() - tempoAtual
+  def remaining_minutes_in_oven(tempoForno) do
+    expected_minutes_to_oven() - tempoForno
   end
+
+  # Tempo para preparar as camadas
+  def preparation_time_in_minutes(camadas) do
+    2 * camadas
+  end
+
+  # Tempo gasto até o momento
+  def total_time_in_minutes(camadas, tempoForno) do
+    tempoForno + preparation_time_in_minutes(camadas)
+  end
+
+  def alarm do
+    "Ding!"
+  end
+
 end
 
 IO.puts "#{Lasagna.expected_minutes_to_oven()}"
-IO.puts "#{Lasagna.remaining_minutes_in_oven(10)}"
+IO.puts "#{Lasagna.remaining_minutes_in_oven(20)}"
+IO.puts "#{Lasagna.preparation_time_in_minutes(3)}"
+IO.puts "#{Lasagna.total_time_in_minutes(3, 20)}"
+IO.puts "#{Lasagna.alarm()}"
